90e07c63312041855aed93e91bacb9908969ecfd
Bhargab jyoti Bhuyan <110892961+BJB0@users.noreply.github.com>
2023-07-09 13:54:53 +0530

    Add files via upload

---
+++ html code.c
@@ -0,0 +1,50 @@
+<!DOCTYPE html >
+<html>
+    <body>
+        <header>
+           
+            <h1>my portfolio</h1>
+            
+
+        </header>
+        <main>
+            <section>
+                education: Tezpur University | school: kvcut
+            </section>
+            <section>
+                experiences: GSOC | Microsoft 
+            </section>
+            <a href="https://www.youtube.com/" target="_main">
+                <img 
+                   src="https://upload.wikimedia.org/wikipedia/commons/e/ef/Youtube_logo.png?20220706172052" 
+                   alt="youtube"
+                   height="100"</a>
+            <aside> This is an ad</aside>
+            <div>
+                <p>hello world</p>
+                <p>welcome to the html world</p>
+                <p>let's begin</p>
+            </div>
+            <div>
+                <p>My name is Bhargab</p>
+                <p>currently learning html</p>
+            </div>
+            
+
+        </main>
+        <footer>
+            <h3>contact me:</h3>
+        </footer>
+
+
+
+
+
+
+
+
+
+    </body>
+    
+
+</html>
